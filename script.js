94434edc082c0d237c1b5b14c2fbb0802cf63764
pkkero <kerolin.ferraz@escola.pr.gov.br>
2026-07-02 09:52:19 -0300

    curtida

---
+++ script.js
@@ -0,0 +1,7 @@
+const curtidas=documnt .getElementById("curtidas")
+curtidas.addEventListener("click",aumentacurtidas)
+
+function aumentacurtidas(){
+    let quantidade =document.querySelector("span")
+    quantidade.textContent++;0
+}
